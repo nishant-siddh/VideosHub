c22e38827dcf1432ad9cdd26a494968f171ee63c
Nishant Siddh <94243692+nishant-siddh@users.noreply.github.com>
2023-07-24 02:01:55 +0530

    fixed routing and user log in state check

--- src/utils/getDataFromToken.js
+++ src/utils/getDataFromToken.js
@@ -2,9 +2,7 @@ import jwt from 'jsonwebtoken';
 
 const getDataFromToken = async (req) => {
     try {
-        console.log('this log is from getDataFromToken');
         const encodedToken = req.cookies.get('token')?.value || "";
-        console.log(encodedToken, 'encodedToken from getDataFromToken');
         const decodedToken = await jwt.verify(encodedToken, process.env.JWT_SECRET);
         return decodedToken.id;
         
